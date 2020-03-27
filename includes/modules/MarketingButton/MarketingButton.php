9a63251e272dc531ca2632f99953ab5798962ea5
Sushma Kure <sushmak@bsf.io>
2020-03-27 18:57:06 +0530

    Updated default toggles for buttn settings

--- includes/modules/MarketingButton/MarketingButton.php
+++ includes/modules/MarketingButton/MarketingButton.php
@@ -7,6 +7,84 @@ class MYEX_MarketingButton extends ET_Builder_Module {
 
 	public function init() {
 		$this->name = esc_html__( 'Marketing Button', 'myex-my-extension' );
+
+		$this->custom_css_fields = array(
+			'main_element' => array(
+				'label'    => esc_html__( 'Main Element', 'myex-my-extension' ),
+				'no_space_before_selector' => true,
+			),
+		);
+
+		$this->settings_modal_toggles = array(
+			'general'  => array(
+				'toggles' => array(
+					'main_constent' => esc_html__( 'Text', 'myex-my-extension' ),
+					'link'         => esc_html__( 'Link', 'myex-my-extension' ),
+				),
+			),
+			'advanced' => array(
+				'toggles' => array(
+					'alignment'  => esc_html__( 'Alignment', 'myex-my-extension' ),
+					'text'       => array(
+						'title'    => esc_html__( 'Text', 'myex-my-extension' ),
+						'priority' => 49,
+					),
+				),
+			),
+		);
+
+		$this->advanced_fields = array(
+			'borders'               => array(
+				'default' => false,
+			),
+			'button'                => array(
+				'button' => array(
+					'label' => esc_html__( 'Button', 'myex-my-extension' ),
+					'css' => array(
+						'main' => $this->main_css_element,
+						'limited_main' => "{$this->main_css_element}.et_pb_button",
+					),
+					'box_shadow'     => false,
+					'margin_padding' => false,
+				),
+			),
+			'margin_padding' => array(
+				'css' => array(
+					'padding' => "{$this->main_css_element}_wrapper {$this->main_css_element}, {$this->main_css_element}_wrapper {$this->main_css_element}:hover",
+					'margin' => "{$this->main_css_element}_wrapper",
+					'important' => 'all',
+				),
+			),
+			'text'                  => array(
+				'use_text_orientation' => false,
+				'use_background_layout' => true,
+				'options' => array(
+					'background_layout' => array(
+						'default_on_front' => 'light',
+						'hover' => 'tabs',
+					),
+				),
+			),
+			'text_shadow'           => array(
+				// Text Shadow settings are already included on button's advanced style
+				'default' => false,
+			),
+			'background'            => false,
+			'fonts'                 => false,
+			'max_width'             => false,
+			'height'                => false,
+			'link_options'          => false,
+			'position_fields'       => array(
+				'css' => array(
+					'main'=> "{$this->main_css_element}_wrapper",
+				),
+			),
+			'transform'             => array(
+				'css' => array(
+					'main' => "{$this->main_css_element}_wrapper",
+				),
+			),
+		);
 	}
 
 	public function get_fields() {
@@ -27,6 +105,16 @@ class MYEX_MarketingButton extends ET_Builder_Module {
 				'show_if'		  => array(
 					'heading' 		=> 'sushma',
 				)
+			),
+			'heading_text_color' => array(
+				'label'             => esc_html__( 'Heading Text Color', 'myex-my-extension' ),
+				'description'       => esc_html__( 'You can pick unique text colors for Heading. Choose the open state title color here.', 'uad-my-extension' ),
+				'type'              => 'color-alpha',
+				'custom_color'      => true,
+				'tab_slug'          => 'advanced',
+				'toggle_slug'       => 'toggle',
+				'hover'             => 'tabs',
+				'mobile_options'    => true,
 			),
 		);
 	}
